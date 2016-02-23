37bd0119674194047060f64bf510c59a140d3363
Barbara Danrel <barbd25@yahoo.com>
2016-02-23 00:11:27 -0500

    fixed issue with extra hyphen at end of password

--- logic.php
+++ logic.php
@@ -7,9 +7,16 @@ $wordlist = ["aardvark", "cougar", "dingo","elephant","sloth","crocodile","emu",
  if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
     $password="";
 	$wordNumber=4; #default value
-	for($i=0; $i<$wordNumber; $i++){
-		$password .= $wordlist[rand(0,9)] . "-";
+	
+	//first word in password 
+	$password = $wordlist[rand(0,count($wordlist)-1)];  
+ 
+	//add additional words as requested in form, words will be separated by '-' 
+	for ($i = 1; $i < $wordNumber; $i++) { 
+		//$password = $password."-".$wordlist[$rand_word[$i]]; 
+		$password = $password."-".$wordlist[rand(0,count($wordlist)-1)]; 
 	}
+	
 	if (isset($_GET['useNumber'])) {
 		// checkbox was checked. 
         $password .= rand(0,9);
