0c48397b9157ace2c8bbcbc003b1d6fc00431584
Barbara Danrel <barbd25@yahoo.com>
2016-02-24 22:05:16 -0500

    modified description to specify the range of # words allowed

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 		<p>Have you ever created a password, but then quickly forgotten what it was? If this has happened to you, an xkcd password might be
 		something to consider. The idea behind it is that a small number of random, common words could make a strong password and be easy to remember 
 		at the same time.</p>
-		<p>Here is a simple example of an xkcd password generator. Enter the number of words you want in the password and select to add a symbol 
-		and/or a number. Then click the button to get a new password.</p><br>
+		<p>Here is a simple example of an xkcd password generator. Enter the number of words you want in the password (between 1 and 9) and select to 
+		include a symbol and/or a number. Then click the button to get a new password.</p><br>
 		
 		<form method='GET' action='index.php'>
 			Number of words (default is 4)  <input type='text' name='wordNumber' maxlength="1"> <br>
