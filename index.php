e203d133103bd77fac3e4d2817eaff7b3091e9d8
Barbara Danrel <barbd25@yahoo.com>
2016-02-24 21:21:56 -0500

    modified text in description

--- index.php
+++ index.php
@@ -10,16 +10,20 @@
 <body>
 	<div class="container">
 		<h1>xkcd Password Generator</h1><br>
-		<p class='description'>Have you ever created a password but then quickly forgotten what it was? If this has happened to you, an xkcd password might be
-		something to consider. The idea behind it is that a small number of random, real words could be just as strong as a complicated one,
-		but be a little easier to remember. Here is a simple example of an xkcd password generator. Enter the number of words you want in the password
-		and select to add a symbol and/or a number. Then click the button to get a new password.</p><br>
+		
+		<p>Have you ever created a password, but then quickly forgotten what it was? If this has happened to you, an xkcd password might be
+		something to consider. The idea behind it is that a small number of random, common words could make a strong password and be easy to remember 
+		at the same time.</p>
+		<p>Here is a simple example of an xkcd password generator. Enter the number of words you want in the password and select to add a symbol 
+		and/or a number. Then click the button to get a new password.</p><br>
+		
 		<form method='GET' action='index.php'>
-			Enter number of words (default is 4)  <input type='text' name='wordNumber' maxlength="1"> <br>
+			Number of words (default is 4)  <input type='text' name='wordNumber' maxlength="1"> <br>
 			<label><input type="checkbox" name="useSymbol"> Include a symbol</label><br>
 			<label><input type="checkbox" name="useNumber"> Include a number</label><br>
 			<input type='submit' value='Get a new password!'><br>
 		</form>
+		
 		<p class='password'>Password is:<br><?php echo $password ?></p><br>
 		
 	</div>
