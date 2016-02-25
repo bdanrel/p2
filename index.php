8fca62244c1a158d8e5b082d396eab085b022c38
Barbara Danrel <barbd25@yahoo.com>
2016-02-24 21:03:32 -0500

    fixed bug with always using default # words, removed debugging, added description

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Barbara Danrel's P2 xkcd Password Generator for DWA-15</title>
 	<meta charset='utf-8'>
+	
+	<link href='css/styles.css' rel='stylesheet' type='text/css'>
 	<?php require('logic.php'); ?>
 </head>
 <body>
-	<form method='GET' action='index.php'>
-		<h1>xkcd Password Generator</h1>
+	<div class="container">
+		<h1>xkcd Password Generator</h1><br>
+		<p class='description'>Have you ever created a password but then quickly forgotten what it was? If this has happened to you, an xkcd password might be
+		something to consider. The idea behind it is that a small number of random, real words could be just as strong as a complicated one,
+		but be a little easier to remember. Here is a simple example of an xkcd password generator. Enter the number of words you want in the password
+		and select to add a symbol and/or a number. Then click the button to get a new password.</p><br>
+		<form method='GET' action='index.php'>
+			Enter number of words (default is 4)  <input type='text' name='wordNumber' maxlength="1"> <br>
+			<label><input type="checkbox" name="useSymbol"> Include a symbol</label><br>
+			<label><input type="checkbox" name="useNumber"> Include a number</label><br>
+			<input type='submit' value='Get a new password!'><br>
+		</form>
+		<p class='password'>Password is:<br><?php echo $password ?></p><br>
 		
-		Enter an integer number of words between 1-9 (defaults to 4 if invalid)  <input type='text' name='wordNumber'> <br>
-		<label><input type="checkbox" name="useSymbol"> Include a special character like @</label><br>
-		<label><input type="checkbox" name="useNumber"> Include a number</label><br>
-		<input type='submit' value='Get a new password!'><br>
-	</form>
-	Your password is <?php echo $password ?>
-
+	</div>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,24 +1,23 @@
 <?php
-var_dump($_GET);
-$wordlist = ["aardvark", "cougar", "dingo","elephant","sloth","crocodile","emu","armadillo","tiger","zebra"];
+$wordlist = ["cougar", "dingo","elephant","sloth","crocodile","emu","armadillo","tiger","zebra",
+"yellow","green","blue","apple","grapes","banana"];
 //declare empty password to start
 $password="";
 
 
-
 if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
-	 
+	
 	//declare $wordNumber with default value of 4 if null
-	$wordNumber = (isset($_GET['wordNumber']) ? htmlspecialchars($_GET['wordNumber']) : 4);
+	$wordNumber = (isset($_GET['wordNumber']) ? $_GET['wordNumber'] : 4);
 	#$wordNumber = trim($wordNumber);  //remove spaces
     //set # words to default value 4 if $wordnum is not between 1 and 9 or is not an integer
 	if ($wordNumber < 1 or $wordNumber > 9 ) { 
 		$wordNumber = 4; 
 	}
-	elseif (!is_numeric($wordNumber) or !is_int($wordNumber)){ 
+	elseif (!is_numeric($wordNumber)){ 
 		$wordNumber = 4; 
 	}
-		
+			
 	//first word in password 
 	$password = $wordlist[rand(0,count($wordlist)-1)];  
  
